9533fa19c400d53455bbcc9614770399376cdb43
Ever Reales <ejreal75@gmail.com>
2026-09-13 13:06:57 -0500

    feat: add route dataset, app template, and label component

--- src/lib/components/label.svelte
+++ src/lib/components/label.svelte
@@ -1,5 +1,5 @@
 <script>
-  export let heading, body, className
+  export let heading, body, className = ''
 </script>
 
 <div class='h-12 w-14 md:h-14 md:w-14 flex flex-col text-center justify-center text-gray-800 dark:text-gray-200 p-1 {className}'>
